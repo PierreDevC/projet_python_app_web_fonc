ad71a61f00b5c35c0e7c72b13e9b49b7f4917fce
PierreDevC <pscypre@gmail.com>
2024-10-29 00:31:21 -0400

    crud changes

---
+++ database/crud_script.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+with sqlite3.connect('produits.db') as conn:
+    cur = conn.cursor()
+    # Insertion d'un produit
+    cur.execute("INSERT INTO produits (nom, categorie, marque, modele, prix, description, stock) VALUES ('iPhone 15 Pro', 'téléphone', 'Apple', 'A2850', 1199.99, ' ', 50)")
+
+    # Affichage de tous les éléments de la table produits
+    cur.execute("SELECT * FROM produits")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    
+    # Mise à jour du produit (ajout d'une description)
+    cur.execute("UPDATE produits SET description = 'Le dernier iPhone avec puce A17 Pro' WHERE nom = 'Iphone 15 Pro'")
+    conn.commit()
+
+    cur.execute('DELETE FROM produits WHERE')
