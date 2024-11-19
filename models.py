0d4a8276a2c63cb19951b529e9aaafe7b87e5fd9
P-S.Cypre <pscypre@gmail.com>
2024-11-19 00:36:02 -0500

    modify product feature 2

--- models.py
+++ models.py
@@ -1,17 +1,5 @@
-import sqlite3
 from datetime import datetime
-from functools import wraps
 
-def db_connection(func):
-    @wraps(func)
-    def wrapper(self, db_path, *args, **kwargs):
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        result = func(self, cursor, *args, **kwargs)
-        conn.commit()
-        conn.close()
-        return result
-    return wrapper
 
 
 class BaseModel:
@@ -19,11 +7,7 @@ class BaseModel:
     # à finir.... va être parent de toutes les classes
 
 
-class Customers:
-    def __init__(self, first_name, last_name, email):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
+
 
 class Products:
     def __init__(self, name, type, category, brand, price, stock, description=None):
@@ -37,36 +21,16 @@ class Products:
         self.date_added = datetime.now().strftime("%Y-%m-%d")
 
 
-    @db_connection
-    def add_product(self, cursor):
-        cursor.execute('''
-            INSERT INTO products (name, type, category, brand, price, stock, description, date_added)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (self.name, self.type, self.category, self.brand, self.price, self.stock, self.description, self.date_added))
-        self.id = cursor.lastrowid
-
-    @db_connection
-    def update_product(self, cursor):
-        cursor.execute('''
-            UPDATE products
-            SET name = ?, type = ?, category = ?, brand = ?, price = ?, stock = ?, description = ?
-            WHERE id = ?
-        ''', (self.name, self.type, self.category, self.brand, self.price, self.stock, self.description, self.id))
-
-    @staticmethod
-    @db_connection
-    def delete_product(cursor, product_id):
-        cursor.execute('DELETE FROM products WHERE id = ?', (product_id,))
-
-    @staticmethod
-    @db_connection
-    def get_product_by_id(cursor, product_id):
-        cursor.execute('SELECT * FROM products WHERE id = ?', (product_id,))
-        return cursor.fetchone()
-
-    @staticmethod
-    @db_connection
-    def get_all_products(cursor):
-        cursor.execute('SELECT * FROM products')
-        return cursor.fetchall()
+class Customers:
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
     
+class Orders:
+    def __init__(self, customer_id, product_id, quantity):
+        self.customer_id = customer_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.date_ordered = datetime.now().strftime("%Y-%m-%d")
+        
